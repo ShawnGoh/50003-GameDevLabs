7367c31edaec7083a7fd8b99ee91b80256fd6db1
Shawn Goh <goh_shawn1998@hotmail.com>
2022-06-08 23:04:02 +0800

    added Lab 3 checkoff

---
+++ Lab 3/Game Dev Lab 3 Checkoff/Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+
+    public TMP_Text gameoverScoreText;
+
+
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+        gameoverScoreText.text = "SCORE: " + score.ToString() +"\n"+"HI-SCORE: "+PlayerPrefs.GetInt("HISCORE",0);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void RestartButton()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Mario Lab Scene");
+    }
+
+    public void MainMenuButton()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Mario Lab Main Menu Scene");
+    }
+}
